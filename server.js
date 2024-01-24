8089765392d9dd801518a0a89df3091620f453bc
hrishabhvyas12 <hrishabh1000@gmail.com>
2024-01-24 22:07:26 +0530

    final

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
-import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
-import { fileURLToPath } from "url";
 
 //configure env
 dotenv.config();
- 
+
 //databse config
 connectDB();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
@@ -25,7 +20,6 @@ const app = express();
 //middelwares
 app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "./app/build")));
 
 //routes
